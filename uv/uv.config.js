439cdd647416c29a41c476b45e627e945fed69eb
Thisisan_althowcool <122313708+Thisisanalthowcool@users.noreply.github.com>
2024-02-24 13:34:23 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/classroom/',
-    bare: 'https://github.com/TheLlamainator/Ultraviolet-Static-Version'
+    bare: 'https://coolbare.lightspeedsucks.workers.dev'
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
